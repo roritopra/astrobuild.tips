f35872bcc6d420d2dd2d5179ec9d5ea0c6f4129d
roritopra <1193102598@u.icesi.edu.co>
2023-07-21 12:50:13 -0500

    [ADD] Rsx config, rss, and Header top react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
-
 import svelte from "@astrojs/svelte";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react(), vue(), svelte()]
+  site: 'https://astrobuild.tips',
+  integrations: [tailwind(), mdx(), react(), vue(), svelte(), sitemap()]
 });
